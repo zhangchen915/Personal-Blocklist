144f4a35aab27b9e64993383dfa2fbf64fb237b3
Chen <zhangchen915@outlook.com>
2018-06-01 15:40:05 +0800

    rebuild: remove zepto

---
+++ webpack-bg.js
@@ -0,0 +1,42 @@
+const path=require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+    entry: {
+        background: './src/content/background.js',
+        content_script: './src/content/content_script.js'
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    resolve: {
+        extensions: ['.js']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ["transform-class-properties"]
+                    }
+                }
+
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new UglifyJSPlugin()
+    ]
+};
